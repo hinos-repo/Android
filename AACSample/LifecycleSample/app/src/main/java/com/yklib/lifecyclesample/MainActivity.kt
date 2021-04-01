60ccff35387ff99ffd3ae72c4c14467b6573c2f1
hinos-repo <60995477+hinos-repo@users.noreply.github.com>
2021-04-01 14:21:03 +0900

    title \ LiveDataSample 업데이트

--- AACSample/LifecycleSample/app/src/main/java/com/yklib/lifecyclesample/MainActivity.kt
+++ AACSample/LifecycleSample/app/src/main/java/com/yklib/lifecyclesample/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.d(TAG, "onCreate: ")
         lifecycle.addObserver(MainActivityObserver())
+
     }
 
     override fun onStart() {

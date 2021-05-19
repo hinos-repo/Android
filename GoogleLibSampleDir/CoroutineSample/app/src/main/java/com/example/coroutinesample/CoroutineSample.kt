c73156de7e1c27761fe296b3320229c11cada1fc
DESKTOP-O5UUGIL\dbsrb <hinos.repo@gmail.com>
2021-05-19 23:00:32 +0900

    title \ 코루틴 공부 시작

---
+++ GoogleLibSampleDir/CoroutineSample/app/src/main/java/com/example/coroutinesample/CoroutineSample.kt
@@ -0,0 +1,12 @@
+package com.example.coroutinesample
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    GlobalScope.launch {
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello,")
+    delay(2000L)
+}
